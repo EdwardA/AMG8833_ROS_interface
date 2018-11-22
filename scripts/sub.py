9cd84ca0f9ab19300b9d22b8ef08ff1e12e7ef19
Edward <ea890@live.mdx.ac.uk>
2018-11-22 17:03:59 +0000

    Added changes to implement Temperature msg type for sensor

--- scripts/sub.py
+++ scripts/sub.py
@@ -1,16 +1,32 @@
-#!/usr/bin/env python
-#This is my test script to read the Hello world example from Rosserial arduino library
+#!/usr/bin/env python2
+#This script reads data from an arduino based temperature sensor and publishes it as a temperature message
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64, Float32, Header
+from sensor_msgs.msg import Temperature
+
+def AMG8833_temp(data):
+	pub = rospy.Publisher('temp_chatter', Temperature, queue_size=10)
+	my_msg = Temperature()
+	my_msg.Header
+	my_msg.temperature = Float64(data)
+	my_msg.variance = 0
+	pub.publish(my_msg)
+	#rospy.loginfo(rospy.get_caller_id() + ' I published', my_msg)
+    
+	
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
+	rospy.loginfo(rospy.get_caller_id() + ' I heard %s', data.data)
+	AMG8833_temp(data)
+    
+    
 
 def ard_listener():
-    rospy.init_node('ard_listen', anonymous=False)
-    rospy.Subscriber('ard_chatter', String, callback)
-    rospy.spin()
-
+	rospy.init_node('ard_listen', anonymous=False)
+	rospy.Subscriber('ard_chatter', Float32, callback)
+	rospy.spin()
+    
+    
 if __name__ == '__main__':
-    ard_listener()
+	ard_listener()
