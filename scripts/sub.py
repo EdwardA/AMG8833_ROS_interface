c9643570863de47a89e61dd8b845e823cb5beb42
Edward <ea890@live.mdx.ac.uk>
2018-11-27 10:26:17 +0000

    Fixed temperature message to print correctly to ROS

--- scripts/sub.py
+++ scripts/sub.py
@@ -2,15 +2,16 @@
 #This script reads data from an arduino based temperature sensor and publishes it as a temperature message
 
 import rospy
-from std_msgs.msg import Float64, Float32, Header
+from std_msgs.msg import Float64, Float32, Header, Time
 from sensor_msgs.msg import Temperature
 
 def AMG8833_temp(data):
 	pub = rospy.Publisher('temp_chatter', Temperature, queue_size=10)
 	my_msg = Temperature()
-	my_msg.Header
-	my_msg.temperature = Float64(data)
-	my_msg.variance = 0
+	my_msg.header.stamp = rospy.Time.now()
+	my_msg.header.frame_id = "temperatureID"
+	my_msg.temperature = data.data
+	#my_msg.variance = 0
 	pub.publish(my_msg)
 	#rospy.loginfo(rospy.get_caller_id() + ' I published', my_msg)
     
